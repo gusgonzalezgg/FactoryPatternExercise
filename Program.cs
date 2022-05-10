5a381c908834ed6308cfa7650cfc7eae97936336
Gustavo Gonzalez <gusgonzalez.gg@gmail.com>
2022-05-10 11:22:01 -0400

    finsihed

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPatternExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! How many wheels would you like? 2 or 4?");
+
+            int tireCount = int.Parse(Console.ReadLine());
+
+            var answer = VehicleFactory.GetVehicle(tireCount);
+
+            answer.Drive();
+
+
+
+        }
+    }
+}
